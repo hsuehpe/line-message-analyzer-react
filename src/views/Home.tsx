b9a9e1b5d6dbc8eddbb7bebb5c7726ec8e94fdfe
hsuehpe <bewitchsky@gmail.com>
2024-10-12 15:19:12 +0800

    refactor: Update imports and formatting in Home component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import rootState from '../store/rootState';
 import { Members, DateMemberMessages } from '../types';
@@ -16,68 +16,60 @@ export default function Home() {
 
   const inputEl = useRef() as React.MutableRefObject<HTMLInputElement>;
   const [root, setRootState] = useRecoilState(rootState);
-  const [memeberNameList, setMemberNameList] = useRecoilState(memberList);
+  const [memberNameList, setMemberNameList] = useRecoilState(memberList);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const handleUploadFile = () => {
     if (inputEl.current) inputEl.current.click();
-  }
-
-  const isContainsMessageActions = (name: string) => {
-    for (let i = 0; i < messageActions.length; i++) {
-      if (name.includes(messageActions[i])) return true;
-    }
-    return false;
   };
 
-  const analyze = (lines: Array<string>) => {
-    let totalDays = 0;
-    let curDate = '';
-    const memberNameList = [] as Array<string>;
-    for (let i = 0; i < lines.length; i ++) {
-      const linesAry = lines[i].split(/(\t)/);
+  const isContainsMessageActions = (name: string) => {
+    return messageActions.some(action => name.includes(action));
+  };
 
-      if (dateReg.test(lines[i])) { // date
-        curDate = lines[i].split('（')[0];
-        dateMemberMessages[curDate] = {};
-        totalDays++;
-      }
+  const processLine = (line: string, curDate: string) => {
+    const linesAry = line.split(/(\t)/);
+    if (messageReg.test(linesAry[0])) {
+      const name = linesAry[2];
+      const message = linesAry[4];
+      if (name) {
+        if (!members[name] && !isContainsMessageActions(name)) {
+          members[name] = {
+            totalMessages: 0,
+            totalStickers: 0,
+            totalPhotos: 0,
+            totalTexts: 0,
+          };
+          memberNameList.push(name);
+        }
 
-      if (messageReg.test(linesAry[0])) { // message
-        const name = linesAry[2];
-        const message = linesAry[4];
-        if (name) {
-          if (!members[name] && !isContainsMessageActions(name)) {
-            members[name] = {
-              totalMessages: 0,
-              totalStickers: 0,
-              totalPhotos: 0,
-              totalTexts: 0,
-            };
-            memberNameList.push(name);
-          }
+        if (!dateMemberMessages[curDate][name]) dateMemberMessages[curDate][name] = { messages: 0 };
 
-          if (!dateMemberMessages[curDate][name]) dateMemberMessages[curDate][name] = { messages: 0 };
+        if (message) {
+          if (message.startsWith('[貼圖]')) members[name].totalStickers++;
+          else if (message.startsWith('[照片]')) members[name].totalPhotos++;
+          else members[name].totalTexts++;
 
-          if (message) {
-            if (message.substring(0, 4) === '[貼圖]') members[name].totalStickers++;
-            else if (message.substring(0, 4) === '[照片]') members[name].totalPhotos++;
-            else members[name].totalTexts++;
-
-            if (dateMemberMessages[curDate][name]) dateMemberMessages[curDate][name].messages++;
-            members[name].totalMessages++;
-          }
+          dateMemberMessages[curDate][name].messages++;
+          members[name].totalMessages++;
         }
       }
     }
+  };
 
-    setRootState(Object.assign({}, {
-      dateMemberMessages,
-      members
-    }));
+  const analyze = (lines: Array<string>) => {
+    let curDate = '';
+    for (const line of lines) {
+      if (dateReg.test(line)) {
+        curDate = line.split('（')[0];
+        dateMemberMessages[curDate] = {};
+      } else {
+        processLine(line, curDate);
+      }
+    }
 
+    setRootState({ dateMemberMessages, members });
     setMemberNameList(memberNameList);
-
     history.push('/analytics');
   };
 
@@ -87,37 +79,31 @@ export default function Home() {
       const file = target.files[0];
       const fr = new FileReader();
       inputEl.current.value = '';
-      fr.onload = function(e: ProgressEvent<FileReader>) {
+      fr.onload = (e: ProgressEvent<FileReader>) => {
         if (e.target) {
-          const result = e.target.result as string
+          const result = e.target.result as string;
           analyze(result.split('\n'));
         }
-      }
+      };
 
-      fr.onloadstart = function() {
-        setIsLoading(true);
-      }
-
-      fr.onloadend = function() {
-        setIsLoading(false);
-      }
-
+      fr.onloadstart = () => setIsLoading(true);
+      fr.onloadend = () => setIsLoading(false);
       fr.readAsText(file);
     }
-  }
+  };
 
   return (
-    <div className="flex justify-center items-center flex-col">
-      <h1 className="ui header">Line 統計分析</h1>
-      <Loader style={ { display: isLoading ? 'block' : 'none' } } />
-      <Segment placeholder className="w-[400px] h-[300px]">
-        <Header icon>
-          <Icon name="file text" />
-          Upload your txt file
-        </Header>
-        <Button primary onClick={ handleUploadFile }>載入聊天</Button>
-        <input ref={ inputEl } type="file" name="file" id="file" style={{ display: 'none' }} onChange={ onChange } />
-      </Segment>
-    </div>
-  )
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="ui header">Line 統計分析</h1>
+        <Loader style={{ display: isLoading ? 'block' : 'none' }} />
+        <Segment placeholder className="w-[400px] h-[300px]">
+          <Header icon>
+            <Icon name="file text" />
+            Upload your txt file
+          </Header>
+          <Button primary onClick={handleUploadFile}>載入聊天</Button>
+          <input ref={inputEl} type="file" name="file" id="file" style={{ display: 'none' }} onChange={onChange} />
+        </Segment>
+      </div>
+  );
 }
